6324e3c092cb00b89eb0e6897a027259a0d82122
apoc64 <sschwedt@gmail.com>
2018-08-13 17:09:31 -0600

    responds to new comment button push

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
   // add your code here
+  $("#new_comment_button").on("click", function() {
+    $("#new_comment_button").css('display', 'none')
+    $("#new_comment").css('display', 'block')
+  });
 })
