37a3e665f3e742f8d96c1a807e0d0cb366a3933a
apoc64 <sschwedt@gmail.com>
2018-08-13 17:23:52 -0600

    adds comments to list

--- source/addComment.js
+++ source/addComment.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
-  // add your code here
   $("#new_comment_button").on("click", function() {
     $("#new_comment_button").css('display', 'none')
     $("#new_comment").css('display', 'block')
   });
+
+  $("#submit-button").on("click", function() {
+    event.preventDefault()
+    const comment = $("#comment-text").val()
+    const author = $("#authorName").val()
+    console.log(comment);
+    console.log(author);
+    $(`<li>${comment}<span class="author">${author}</li>`).appendTo("#comment_list")
+  })
 })
