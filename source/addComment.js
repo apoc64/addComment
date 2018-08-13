933a3aa90c9f9689a005364fd5ad50666aae7aa4
apoc64 <sschwedt@gmail.com>
2018-08-13 17:27:38 -0600

    hides form after submit, redisplays new comment button, and blanks form fields

--- source/addComment.js
+++ source/addComment.js
@@ -8,8 +8,10 @@ $(document).ready(function(){
     event.preventDefault()
     const comment = $("#comment-text").val()
     const author = $("#authorName").val()
-    console.log(comment);
-    console.log(author);
     $(`<li>${comment}<span class="author">${author}</li>`).appendTo("#comment_list")
+    $("#new_comment_button").css('display', 'block')
+    $("#new_comment").css('display', 'none')
+    $("#comment-text").val("")
+    $("#authorName").val("")
   })
 })
